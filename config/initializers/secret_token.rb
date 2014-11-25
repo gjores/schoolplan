2c6e21e17720377e758bc71da0d57c40753f2f80
gjorPe <gjores@gmail.com>
2014-11-25 18:30:53 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Schoolplan::Application.config.secret_key_base = '682fd6534f0fa910ecdf33d2e60a19f954d6e399d43ba3e1465383c62f857e91a32bed0d4e891df463b8c022ab3ed58aeca56b92490ba04cd8d9be42f4fd67c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Schoolplan::Application.config.session_store :cookie_store, key: '_schoolplan_session'
